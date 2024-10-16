af8dd4bb699039e5b88eb2124a396c2e2aad5681
Horhe <devzakrzewski@gmail.com>
2024-10-16 11:38:30 -0700

    Add frontend and backend files

---
+++ backend/src/main/java/com/example/bitrixsalesmansuport/BitrixSalesmanSupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitrixsalesmansuport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitrixSalesmanSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitrixSalesmanSupportApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/bitrixsalesmansuport/BitrixSalesmanSuportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitrixsalesmansuport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitrixSalesmanSuportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
